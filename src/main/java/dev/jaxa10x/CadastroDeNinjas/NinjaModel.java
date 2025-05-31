e1f1561303727fdaef02a878204f3cedd6389197
matheussversutti <sversuttimatheus@gmail.com>
2025-05-31 10:59:07 -0300

    Identidade ninja criada

---
+++ src/main/java/dev/jaxa10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,51 @@
+package dev.jaxa10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//Entity ele transforma a classe em uma tabela no banco de dados
+// JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Aqui vou passtar uma estrategia de como eu vou gerar o ID)
+     private Long id;
+
+     private String nome;
+     private String email;
+     private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
